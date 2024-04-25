88efbcfc721185a8168362bef80eaf323ef9439e
binhvu284 <131544066+binhvu284@users.noreply.github.com>
2024-04-25 21:48:00 +0700

    Add files via upload

---
+++ scripts/frontend/home page.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Smart</title>
+    <link rel="icon" href="./src/logo.png" type="image/png">
+    <link rel="stylesheet" type="text/css" href="./style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+<body>
+    <!-- Navigation bar -->
+    <nav class="navbar navbar-expand-sm .sticky-top shadow-sm">
+        <!-- Logo -->
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="./src/logo.png" width="100px">
+              </a>
+        
+        <!-- Menu section -->
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav nav-pills ">
+          <!-- Course -->
+          <li class="nav-item">
+            <a class="nav-link" href="#">Course</a>
+          </li>
+          <!-- Take test -->
+          <li class="nav-item">
+            <a class="nav-link" href="#">Take test</a>
+          </li>
+        </ul>
+      </div>
+      <!-- Sign in sign up button -->
+      <div class="d-flex">
+        <button class="access-button login-button" onclick="window.location.href='dashboard.html'">Login</button>
+        <button class="access-button sign-up-button">Sign up</button>
+      </div>
+    </div>
+
+    </div>
+    </nav>
+</body>
+</html>

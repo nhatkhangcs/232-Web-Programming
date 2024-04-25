9566ac8c866f1cf85397cd21ce8ae3ce37afb176
binhvu284 <131544066+binhvu284@users.noreply.github.com>
2024-04-25 21:57:59 +0700

    Update home page.php

--- scripts/frontend/home page.php
+++ scripts/frontend/home page.php
@@ -1,25 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test Smart</title>
-    <link rel="icon" href="./src/logo.png" type="image/png">
-    <link rel="stylesheet" type="text/css" href="../style/style.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Test Smart</title>
+  <link rel="icon" href="./src/logo.png" type="image/png">
+  <link rel="stylesheet" type="text/css" href="../styles/style.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
 </head>
+
 <body>
-    <!-- Navigation bar -->
-    <nav class="navbar navbar-expand-sm .sticky-top shadow-sm">
-        <!-- Logo -->
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <img src="./src/logo.png" width="100px">
-              </a>
-        
-        <!-- Menu section -->
+  <!-- Navigation bar -->
+  <nav class="navbar navbar-expand-sm .sticky-top shadow-sm">
+    <!-- Logo -->
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="./src/logo.png" width="100px">
+      </a>
+
+      <!-- Menu section -->
       <div class="collapse navbar-collapse" id="collapsibleNavbar">
         <ul class="navbar-nav nav-pills ">
           <!-- Course -->
@@ -40,6 +42,7 @@
     </div>
 
     </div>
-    </nav>
+  </nav>
 </body>
+
 </html>
